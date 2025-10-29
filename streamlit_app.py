597be9728b154f2ce3eff98038d3e3da6643e073
jumanasheana-glitch <jumanasheana@gmail.com>
2025-10-29 10:59:41 -0400

    added last name to page title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 from datatime import datetime
 
-# page Config
+# Page Config
 st.set_page_config(
-  page_title = 'Asheana | Portfolio',
+  page_title = 'Asheana Juman | Portfolio',
   page_icon='🎯'
   layout = 'wide'
 )
