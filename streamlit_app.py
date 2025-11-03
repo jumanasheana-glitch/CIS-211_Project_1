ac46047de441a691f81824c2dfac088776fe0bd1
jumanasheana-glitch <jumanasheana@gmail.com>
2025-11-03 10:25:49 -0500

    Update nav bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,16 @@ st.set_page_config(
   page_icon='🎯',
   layout = 'wide'
 )
+
+# Custom CSS (optional - for styling)
+st.markdown('''
+                <style>
+                    .main-header {font-size: 42px; font-weight: bold; text-align:center;}
+                    .sub-header {font_size: 24px; text-align:center; color: #666;}
+                </style>
+            ''', unsafe_allow_html = True) 
+
+# Sidebar
+st.sidebar.title('📍 Navigation')
+page = st.sidebar.radio('Go to',
+                        ['🏠 Home', '🤠 About', '💼 Projects', '🛠 Skills', '📄 Resume', '📩 Contact'])
